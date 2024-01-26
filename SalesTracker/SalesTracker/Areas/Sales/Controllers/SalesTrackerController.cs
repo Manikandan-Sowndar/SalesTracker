662fd32fb9fd79a9251436beecbedcd8c28a0e3d
Manikandan S <manikandan.sowndar@gmail.com>
2024-01-26 21:01:03 +0530

    Added project to repository

---
+++ SalesTracker/SalesTracker/Areas/Sales/Controllers/SalesTrackerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SalesTracker.Areas.Sales.Controllers
+{
+    public class SalesTrackerController : Controller
+    {
+        // GET: Sales/SalesTracker
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Billing
+        public ActionResult Billing()
+        {
+            return View();
+        }
+
+        // Report
+        public ActionResult Report()
+        {
+            return View();
+        }
+    }
+}
